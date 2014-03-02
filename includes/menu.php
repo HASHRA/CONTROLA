0d50848d3b1c9d72732ca9b0a963f71410bc17ea
Max van Anen <maxzilla@gmail.com>
2014-03-02 22:20:40 +0100

    fixed dashboard link

--- includes/menu.php
+++ includes/menu.php
@@ -1,7 +1,7 @@
 <!-- SIDEBAR -->
                 <aside class="sidebar">
                     <ul class="nav nav-stacked">
-                        <li class="active" ><a href="index.html"><i class="fa fa-dashboard fa-fw"></i>Dashboard</a></li>
+                        <li class="active" ><a href="/"><i class="fa fa-dashboard fa-fw"></i>Dashboard</a></li>
                         <li  ><a href="log.php"><i class="fa fa-terminal fa-fw"></i>Logs</a></li>
                         <li  ><a href="help.php"><i class="fa fa-info fa-fw"></i>Help</a></li>
                         <li  ><a href="update.php"><i class="fa fa-download fa-fw"></i>Update firmware</a></li>
