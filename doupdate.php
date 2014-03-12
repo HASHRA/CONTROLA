c54ad73946a90768bd48719618bf75229d1e6109
Max van Anen <maxzilla@gmail.com>
2014-03-12 22:22:06 +0100

    reboot after update

--- doupdate.php
+++ doupdate.php
@@ -24,8 +24,7 @@ Wait for it!!!.....Close this dialog at your own risk!<br/>
 		
 		require 'config/define.php';
 		echo "Update completed! you now have version <strong> ". VERSION ." </strong><br/>";
-		
-		
+		exec("sudo reboot");
 	?>
 
 	The system is rebooting, when you close this dialog, you will be redirected to the homepage. <br/>
