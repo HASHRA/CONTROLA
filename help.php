d3e869f3a731230fc2311c017802b47c5ab11af3
Max van Anen <maxzilla@gmail.com>
2014-03-15 14:54:20 +0100

    changed help

--- help.php
+++ help.php
@@ -36,7 +36,7 @@
         <div class="col-md-12">
             <div class="panel ">
                 <div class="panel-heading">
-                    <h3 class="panel-title">Fetching</h3>
+                    <h3 class="panel-title">Work in progress</h3>
                 </div>
                 <div class="panel-body">
                 	Work in progress                	
