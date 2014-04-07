1f5f9b6c49fdaf1680e089799e2a492ee6fb0d2e
Max van Anen <maxzilla@gmail.com>
2014-04-07 19:28:08 +0200

    just whitespace

--- config/define.php
+++ config/define.php
@@ -23,7 +23,6 @@ define('PATH_TEMP', '/tmp');
 
 define('PATH_LOG', '/var/log');
 
-
 define('FILE_CONFIG', PATH_CONFIG.'/config.ini');
 define('FILE_SYSTEM_SETTNGS', PATH_CONFIG.'/systemsettings.json');
 define('FILE_POOLSETTINGS', PATH_CONFIG.'/poolsettings.json');
