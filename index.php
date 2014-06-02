6fd634441af551ba0d19e574df85a2e84ba4d021
M.F. VAN ANEN <maxzilla@gmail.com>
2014-06-03 00:09:24 +0200

    making things easier to maintain

--- index.php
+++ index.php
@@ -252,7 +252,7 @@ $prodname = PRODUCT_NAME;
                  <div class="panel-body">
                  	<div class="form-group">
 		                	<label for="freq">Core clock speed (Mhz)</label>
-		                	<select class="form-control" id="freq" name="freq" data-toggle="tooltip" data-trigger="focus" title="" data-placement="auto left" data-container="body" type="text" data-original-title="The default clock speed setting for the <?= $prodname ?> is <?= DEFAULT_CLOCK ?>. Change clock speed at your own risk. Increasing Clock speed voids warranty.">
+		                	<select class="form-control" id="freq" name="freq" data-toggle="tooltip" data-trigger="focus" title="" data-placement="auto left" data-container="body" type="text" data-original-title="The default clock speed setting is <?= DEFAULT_CLOCK ?>. Change clock speed at your own risk. Increasing Clock speed voids warranty.">
 								<?php for($i = 250 ; $i <= 350 ; $i++) {?>
 									<option value="<?php echo $i?>" <?php $tbool = $freq == $i ? 'selected="selected"' : ''; echo $tbool; ?> ><?php echo $i?></option>
 								<?php }?>
